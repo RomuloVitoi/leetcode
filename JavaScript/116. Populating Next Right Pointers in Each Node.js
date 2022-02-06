5ac013b332470fde24e95925e271c113f6b34db6
Romulo Vitoi <romvitoi@gmail.com>
2022-02-06 16:16:14 -0300

    [JS] 116

---
+++ JavaScript/116. Populating Next Right Pointers in Each Node.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  const dfs = (node) => {
+    if (node.left) {
+      node.left.next = node.right;
+
+      if (node.next) {
+        node.right.next = node.next.left;
+      }
+
+      dfs(node.left);
+      dfs(node.right);
+    }
+  }
+
+  if (root) {
+    dfs(root);
+  }
+  
+  return root;
+};
